6e7c27855809cff513216b402bd3a53f0377a93b
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-11-08 18:24:45 +0200

    -Refactoring -Add debug mode on server side

---
+++ src/server-public/app-view/home/home.controller.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('starterApp')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['UserService', '$rootScope'];
+    function HomeController(UserService, $rootScope) {
+        var vm = this;
+
+        vm.user = null;
+
+        initController();
+
+        function initController() {
+            loadCurrentUser();
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                    vm.user = user;
+                });
+        }
+    }
+
+})();
