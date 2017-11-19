3fec0be81efa48f41773a50c40a99393a022f72e
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-11-19 03:27:21 +0200

    add on config the basic tables and initialize them on db model

--- src/server/config.js
+++ src/server/config.js
@@ -4,13 +4,20 @@ module.exports = {
     db: {
         listenPort: 28015,
         host:       'localhost',
-        defaultName: 'pushUP'
+        defaultName: 'pushUP',
+        defaultTables: [
+            {table : 'accounts', key : 'email'},
+            {table : 'groups'  , key : 'id'   },
+            {table : 'sockets' , key : 'id'   }
+        ]
     },
     server: {
-        listenPort: 3000,                                   // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
-        securePort: 8433,                                   // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
-        distFolder: path.resolve(__dirname, '../client'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
-        staticUrl: '/static',                               // The base url from which we serve static files (such as js, css and images)
-        cookieSecret: 'angular-app'                         // The secret for encrypting the cookie
+        listenPort: 3000,                                       // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
+        listenAddress: '192.168.10.5',
+        listenAddressVPN: '147.52.151.55',
+        securePort: 8433,                                       // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
+        distFolder: path.resolve(__dirname, '../client'),       // The folder that contains the application files (note that the files are in a different repository) - relative to this file
+        staticUrl: '/static',                                   // The base url from which we serve static files (such as js, css and images)
+        cookieSecret: 'angular-app'                             // The secret for encrypting the cookie
     }
 };
--- src/server/lib/services/database.service.js
+++ src/server/lib/services/database.service.js
@@ -5,7 +5,8 @@ var debug = require('debug')('pushup-refactoring:server');
 var config = require('../../config');
 
 class database {
-    setupDb(table,key) {
+
+    initDb(){
         var self = this;
         async.waterfall([
             function(callback) {
@@ -17,22 +18,65 @@ class database {
                 });
             },
             function(connection, callback) {
-                rethinkdb.dbCreate('pushUP').run(connection, function(err, result) {
+                rethinkdb.dbCreate(config.db.defaultName).run(connection, function(err, result) {
                     if (err) {
-                        debug("Database with name'pushUP' already created");
+                        debug("Database with name \'" + config.db.defaultName + "\' already created");
                     } else {
-                        console.log("Created new database with name 'pushUP'");
+                        console.log("Created new database with name \'"+ config.db.defaultName + "\'");
                     }
                     callback(null, connection);
                 });
             },
             function(connection, callback) {
-                rethinkdb.db('pushUP').tableCreate(table, {primaryKey: key}).run(connection, function (err, result) {
+                for (const i in config.db.defaultTables){
+                    const table = config.db.defaultTables[i].table;
+                    const key = config.db.defaultTables[i].key;
+                    rethinkdb.db(config.db.defaultName).tableCreate(table, {primaryKey: key}).run(connection, function (err, result) {
+                        if (err) {
+                            debug("table with name '" + table + "' already created");
+                        } else {
+                            console.log("Created new table '" + table + "' with primary key '" + key +"' ");
+                        }
+                        if(i === (config.db.defaultTables.length -1) ){
+                            connection.close();
+                            callback(null, "Database is setup successfully");
+                        }
+                    });
+                }
+            }
+        ], function(err, data) {
+            debug(data);
+        });
+    }
+
+    initTable(table,key) {
+        var self = this;
+        async.waterfall([
+            function(callback) {
+                self.connectToRethinkDbServer(function(err, connection) {
+                    if (err) {
+                        return callback(true, "Error in connecting RethinkDB");
+                    }
+                    callback(null, connection);
+                });
+            },
+            function(connection, callback) {
+                rethinkdb.dbCreate(config.db.defaultName).run(connection, function(err, result) {
+                    if (err) {
+                        debug("Database with name \'" + config.db.defaultName + "\' already created");
+                    } else {
+                        console.log("Created new database with name \'"+ config.db.defaultName + "\'");
+                    }
+                    callback(null, connection);
+                });
+            },
+            function(connection, callback) {
+                rethinkdb.db(config.db.defaultName).tableCreate(table, {primaryKey: key}).run(connection, function (err, result) {
                     connection.close();
                     if (err) {
                         debug("table with name '" + table + "' already created");
                     } else {
-                        console.log("Created new table with name '" + table + "' and primary key '" + key +"' ");
+                        console.log("Created new table '" + table + "' with primary key '" + key +"' ");
                     }
                     callback(null, "Database is setup successfully");
                 });
