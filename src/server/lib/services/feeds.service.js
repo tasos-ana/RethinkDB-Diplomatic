ba8e9d7f32fe66f6d19eab699e7b569f1f6ca95a
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-11-20 14:11:50 +0200

    refactor feed function add the field socket on each table and remove the sockets table

--- src/server/lib/services/feeds.service.js
+++ src/server/lib/services/feeds.service.js
@@ -2,40 +2,64 @@ var rethinkdb = require('rethinkdb');
 var db = require('./database.service');
 var dbModel = new db();
 var debug = require('debug')('pushup-refactoring:server');
-
+var async = require('async');
 
 module.exports = function(socket) {
     socket.on('feed', function (table) {
-        dbModel.validSocket(table,function (err,enabled) {
-            if(err){
-                debug('Error cant initialize socket on \'' + table + '\'');
-                return;
-            }
-            if(!enabled){
-                debug('feeds on socket: ' + table);
-                dbModel.connectToDb(function(err,connection) {
+        async.waterfall([
+            function(callback) {
+                dbModel.connectToDb(function(err, connection) {
                     if (err) {
-                        return callback(true, "Error connecting to database");
+                        return callback(true, "Error in connecting RethinkDB");
                     }
-
-                    rethinkdb.table(table).changes().run(connection,function(err,cursor) {
-                        if(err) {
-                            console.log(err);
-                        }
-                        cursor.each(function(err,row) {
+                    callback(null, connection);
+                });
+            },
+            function (connection, callback) {
+                rethinkdb.table(table).get('socket').update({connected: true})
+                    .run(connection,function (err,result) {
+                       if(err){
+                           return callback(true,'Error happens while retrieved socket from table',connection);
+                       }
+                       if(result.replaced === 1){
+                           callback(null,connection);
+                       }else{
+                           return callback(true,'Error already listen socket',connection);
+                       }
+                    });
+            },
+            function (connection, callback) {
+                rethinkdb.table(table).changes().run(connection,function(err,cursor) {
+                    if(err) {
+                        return callback(true,'Error happens while waiting for changes',connection);
+                    }
+                    cursor.each(function(err,row) {
+                        if(row !== undefined){
                             console.log(JSON.stringify(row));
                             if(Object.keys(row).length > 0) {
-                                socket.broadcast.emit(table,{
-                                    "data"  : row.new_val.data,
-                                    "id"    : row.new_val.id,
-                                    "time"  : row.new_val.time,
-                                    "type"  : row.new_val.type});
+                                if(row.new_val.id === 'socket'){
+                                    // cursor.close(function (err) {
+                                    //     if(err){
+                                    //         debug('Error happens on cursor close');
+                                    //     }
+                                    // });
+                                    return callback(true,'Socket stop listen on ' + table,connection);
+                                }else{
+                                    socket.broadcast.emit(table,{
+                                        "data"  : row.new_val.data,
+                                        "id"    : row.new_val.id,
+                                        "time"  : row.new_val.time,
+                                        "type"  : row.new_val.type
+                                    });
+                                }
                             }
-                        });
+                        }
                     });
-
                 });
             }
+        ], function(err, msg, connection) {
+            connection.close();
+            debug(msg);
         });
     });
 };
