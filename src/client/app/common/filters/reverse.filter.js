df260759f6d3661dea0e04e93f2c1e0456e9dc2e
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-11-25 01:41:12 +0200

    move reverse filter on separate  file

---
+++ src/client/app/common/filters/reverse.filter.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('starterApp')
+        .filter('reverse',reverse)
+
+    reverse.$inject =[];
+    function reverse() {
+        return function(items) {
+            if(typeof items === 'undefined') { return; }
+            return angular.isArray(items) ?
+                items.slice().reverse() : // If it is an array, split and reverse it
+                (items + '').split('').reverse().join(''); // else make it a string (if it isn't already), and reverse it
+        };
+    }
+
+})();
