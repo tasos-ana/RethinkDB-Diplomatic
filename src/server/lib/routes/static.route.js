38fc58c47cbf5fe3813ab24907d2a833436b6961
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-11-30 03:54:05 +0200

    todo added

--- src/server/lib/routes/static.route.js
+++ src/server/lib/routes/static.route.js
@@ -7,9 +7,6 @@ exports.addRoutes = function(app, config) {
     app.use(favicon(config.server.distFolder + '/favicon.ico'));
 
     // First looks for a static file: index.html, css, images, etc.
-    app.use(config.server.staticUrl, compression());
+    app.use(config.server.staticUrl, compression());//TODO na dw ean prepei na uparxei afto
     app.use(express.static(config.server.distFolder));
-    app.use(config.server.staticUrl, function(req, res, next) {
-        res.send(404); // If we get here then the request for a static file is invalid
-    });
 };
