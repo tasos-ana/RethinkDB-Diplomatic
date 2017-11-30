dedc9ecd7cd7062bd1957b590377af207e6d4fab
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-11-30 03:50:27 +0200

    remove set credential function, cookie will be initialized from server

--- src/client/app/components/login/login.service.js
+++ src/client/app/components/login/login.service.js
@@ -5,37 +5,17 @@
         .module('starterApp')
         .factory('loginService', loginService);
 
-    loginService.$inject = ['$http', '$cookies', '$rootScope', 'socketService'];
-    function loginService($http, $cookies, $rootScope, socketService) {
-        var service = {};
+    loginService.$inject = ['$cookies', 'socketService'];
+    function loginService( $cookies, socketService) {
+        const service = {};
 
-        service.setCredentials = setCredentials;
         service.clearCredentials = clearCredentials;
 
         return service;
 
-        function setCredentials(uEmail, cookie) {
-
-            $rootScope.globals = {
-              currentUser: {
-                  email     : uEmail,
-                  authdata  : cookie}
-            };
-
-            // set default auth header for http requests
-            $http.defaults.headers.common['Authorization'] = cookie;
-
-            // store user details in globals cookie that keeps user logged in for 1 week (or until they logout)
-            const cookieExp = new Date();
-            cookieExp.setHours(cookieExp.getHours() + 1);
-            $cookies.putObject('LOGIN INFO', $rootScope.globals, { expires: cookieExp });
-        }
-
         function clearCredentials() {
             socketService.logout();
-            $rootScope.globals = {};
-            $cookies.remove('LOGIN INFO');
-            $http.defaults.headers.common.Authorization = 'Basic';
+            $cookies.remove('userCredentials');
         }
     }
 })();
