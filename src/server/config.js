325478a1f36ef7aa5bea055f2a416c7c41774dc7
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-12-15 15:52:51 +0200

    Replace external library at client from bower to npm.

--- src/server/config.js
+++ src/server/config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     server: {
         listenPort      : 3000,                                 // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
-        listenAddress   : '192.168.1.78',
+        listenAddress   : '192.168.1.68',
         listenAddressVPN: '147.52.151.51',
         //securePort      : 8433,                                 // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
         securePort      : 8000,
