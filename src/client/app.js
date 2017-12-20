786617004b876c12c47cdb89b635a425a5d1d847
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-12-20 23:41:36 +0200

    add ngAnimate

--- src/client/app.js
+++ src/client/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     angular
-        .module('starterApp',['ngRoute', 'ngCookies', 'angular-md5'])
+        .module('starterApp',['ngRoute', 'ngCookies', 'ngAnimate', 'angular-md5'])
         .config(config)
         .run(run);
 
