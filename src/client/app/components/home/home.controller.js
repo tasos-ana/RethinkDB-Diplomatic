83bfd9c3834426cd84858e91801cf8b51cc61c3a
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-12-22 02:00:10 +0200

    add on homeService function to retrieve user details

--- src/client/app/components/home/home.controller.js
+++ src/client/app/components/home/home.controller.js
@@ -5,13 +5,13 @@
         .module('starterApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$rootScope', '$location', 'httpService', 'socketService', '$timeout'];
-    function HomeController($rootScope, $location, httpService, socketService, $timeout) {
+    HomeController.$inject = ['$rootScope', '$location', 'homeService'];
+    function HomeController($rootScope, $location, homeService) {
         const vm = this;
 
         (function initController() {
             vm.templateURL = $location.path();
+            homeService.retrieveAccountDetails(function () {});
         })();
-
     }
 })();
---
+++ src/client/app/components/home/home.service.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('starterApp')
+        .factory('homeService', homeService);
+
+    homeService.$inject = ['$rootScope', '$location', 'httpService', '$timeout'];
+    function homeService($rootScope, $location, httpService, $timeout) {
+        const service = {};
+
+        service.retrieveAccountDetails = retrieveAccountDetails;
+
+        return service;
+        
+        function retrieveAccountDetails(cb) {
+            if($rootScope.user === undefined || $rootScope.user ===null){
+                httpService.accountGetUserInfo(undefined)
+                    .then(function (response) {
+                        if(response.success){
+                            $rootScope.user = response.data;
+                            $rootScope.user.openedGroupsList = [];
+                            $rootScope.user.activeGroup = undefined;
+                            cb();
+                        }else{
+                            $location.path('/login');
+                        }
+                    });
+            }
+        }
+    }
+})();
