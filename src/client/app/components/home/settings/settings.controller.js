36ee0545188df9fb227bd10cf1985a52a2ce827f
Tasos Anastasas <csd3166@csd.uoc.gr>
2017-12-24 19:27:47 +0200

    add on controller checking to recognize on what attribute we will make an account update, also check if there is an error and saw accordingly notifications

--- src/client/app/components/home/settings/settings.controller.js
+++ src/client/app/components/home/settings/settings.controller.js
@@ -5,8 +5,8 @@
         .module('starterApp')
         .controller('SettingsController', SettingsController);
 
-    SettingsController.$inject = ['$rootScope', '$location', 'homeService', 'dashboardService','socketService', 'httpService', '$timeout', 'notify'];
-    function SettingsController($rootScope, $location, homeService, dashboardService, socketService, httpService, $timeout, notify) {
+    SettingsController.$inject = ['$rootScope', '$scope', '$location', 'homeService', 'dashboardService','socketService', 'httpService', '$timeout', 'notify', '$window', 'settingsService'];
+    function SettingsController($rootScope, $scope, $location, homeService, dashboardService, socketService, httpService, $timeout, notify, $window, settingsService) {
         const vm = this;
 
         vm.updateGroupName  = _updateGroupName;
@@ -15,11 +15,18 @@
         vm.updateAccount    = _updateAccount;
         
         (function initController() {
+            vm.templateURL = $location.path();
             notify.config({duration:'4000', position:'center'});
+
+            //Required for group settings
             $rootScope.editGroup = {};
             $rootScope.deleteGroup = {};
+
+
+            //Required for account settings
             vm.accountSettings = {};
-            vm.templateURL = $location.path();
+            vm.accountSettings.applyChanges = false;
+
             homeService.retrieveAccountDetails(dashboardService.retrieveGroupsName);
         })();
 
@@ -63,8 +70,65 @@
 
 
         //Required for account.settings.view
-        function _updateAccount() {
 
+        function _updateAccount() {
+            //Clean up
+            const newNickname           = vm.accountSettings.newNickname;
+            const curPassword           = vm.accountSettings.curPassword;
+            const newPassword           = vm.accountSettings.newPassword;
+            const confirmNewPassword    = vm.accountSettings.confirmNewPassword;
+            let changeNickname  = false;
+            let changePassword  = false;
+            let error           = false;
+            if($scope.accountSettingsForm.newNickname.$invalid){
+                notify({ message:"Invalid nickname. Please try again", classes :'bg-dark border-danger text-danger'});
+                $window.document.getElementById('newNickname_input').focus();
+                error = true;
+            }else if(newNickname!==undefined && newNickname.length>0){
+                if(curPassword!==undefined && curPassword.length>=8){
+                    changeNickname = true;
+                }else if($scope.accountSettingsForm.curPassword.$valid){
+                    notify({ message:"Your current password is required", classes :'bg-dark border-danger text-danger'});
+                    $window.document.getElementById('curPassword_input').focus();
+                    error=true;
+                }else{
+                    notify({ message:"Your current password is invalid", classes :'bg-dark border-danger text-danger'});
+                    $window.document.getElementById('curPassword_input').focus();
+                    error=true
+                }
+            }
+
+            if($scope.accountSettingsForm.newPassword.$invalid){
+                notify({ message:"Invalid password. Please try again", classes :'bg-dark border-danger text-danger'});
+                $window.document.getElementById('newPassword_input').focus();
+                error=true;
+            }else if($scope.accountSettingsForm.confirmNewPassword.$invalid){
+                notify({ message:"Your passwords don't matched. Please try again", classes :'bg-dark border-danger text-danger'});
+                $window.document.getElementById('confirmNewPassword_input').focus();
+                error=true;
+            }else if(newPassword!==undefined && newPassword.length>0 && confirmNewPassword!==undefined && confirmNewPassword.length>0){
+                if(curPassword!==undefined && curPassword.length>0){
+                    changePassword=true;
+                }else{
+                    notify({ message:"Your current password is required", classes :'bg-dark border-danger text-danger'});
+                    $window.document.getElementById('curPassword_input').focus();
+                    error=true;
+                }
+            }
+
+            if(!error && changeNickname && changePassword){
+                settingsService.updateAccount(vm);
+            }else if(!error && changeNickname){
+                settingsService.updateAccountNickame(vm);
+            }else if(!error && changePassword){
+               settingsService.updateAccountPassword(vm);
+            }else{
+                if(!error){
+                    notify({ message:"You are happy with your details. No changes was made on your account", classes :'bg-dark border-success text-success'});
+                }
+            }
         }
+
+
     }
 })();
--- src/client/app/components/home/settings/settings.service.js
+++ src/client/app/components/home/settings/settings.service.js
@@ -9,7 +9,35 @@
     function settingsService($rootScope, $location, httpService) {
         const service = {};
 
+        service.updateAccountNickame    = _updateAccountNickname;
+        service.updateAccountPassword   = _updateAccountPassword;
+        service.updateAccount           = _updateAccount;
+
         return service;
+
+        function _updateAccountNickname(vm) {
+
+            _clearAccountSettingsForm(vm);
+        }
+        
+        function _updateAccountPassword(vm) {
+
+            _clearAccountSettingsForm(vm);
+        }
+
+        function _updateAccount(vm) {
+
+            _clearAccountSettingsForm(vm);
+        }
+
+        function _clearAccountSettingsForm(vm) {
+            // call that on success update
+            delete vm.accountSettings;
+            vm.accountSettings = {};
+            vm.accountSettings.applyChanges = false;
+            vm.accountSettingsForm.$setPristine();
+        }
+
 
     }
 })();
