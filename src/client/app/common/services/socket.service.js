1427468fa868a0d0dcdd713c9ba7b9e8a05117f3
Tasos Anastasas <csd3166@csd.uoc.gr>
2018-01-29 19:44:36 +0200

    add emit on group create

--- src/client/app/common/services/socket.service.js
+++ src/client/app/common/services/socket.service.js
@@ -197,6 +197,7 @@
                                 $rootScope.user.unreadMessages[data.gID] = 0;
                                 dashboardService.groupOpen(data.gID);
                                 _emitOpenGroup(data.gID);
+                                _emitLastActiveGroup(data.gID);
                                 ngNotify.dismiss();
                                 ngNotify.set("New group created with name '" + data.gName +"'.", "notice-success");
                             }
