9c0c41878430d516fd14eedd9a7edda286757092
Tasos Anastasas <csd3166@csd.uoc.gr>
2018-02-03 17:34:34 +0200

    add button for login

--- android_project/PushUP/app/src/main/java/com/diplomatic/pushup/MainActivity.java
+++ android_project/PushUP/app/src/main/java/com/diplomatic/pushup/MainActivity.java
@@ -1,9 +1,12 @@
 package com.diplomatic.pushup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button) findViewById(R.id.goSignInActivity);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+//        Button signUpBtn = (Button) findViewById(R.id.goSignUpActivity);
+//        signUpBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(view.getContext(), SignupActivity.class);
+//                startActivity(i);
+//            }
+//        });
     }
 
     @Override
